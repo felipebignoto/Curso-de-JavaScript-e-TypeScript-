5be4335dca10a7466d4dba54eb33532ae560f3dd
Felipe-Bignoto <felipebignoto@gmail.com>
2023-01-19 09:12:51 -0300

    teste

---
+++ 021- Inicio de JS (JS-1)/index.js
@@ -0,0 +1,12 @@
+/*Forma de exibit textos - strings */
+/*Quebra a linha automaticamente */
+console.log('texto');
+console.log("Texto");
+console.log(`teste`);
+console.log('texto do "Felipe"');
+console.log("texto do 'Felipe'");
+console.log(`"teste" do 'Felipe'`);
+
+/*Exibindo os numeros - number*/
+console.log(1);
+console.log(1, 2, 3, 4, 8.58, "texto");
