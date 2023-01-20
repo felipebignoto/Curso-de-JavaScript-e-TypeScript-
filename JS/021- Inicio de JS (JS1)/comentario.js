e653024cef7988aac41a798606658aa054c0a4b1
Felipe-Bignoto <felipebignoto@gmail.com>
2023-01-20 10:21:50 -0300

    iniciar aula 13

---
+++ JS/021- Inicio de JS (JS1)/comentario.js
@@ -0,0 +1,11 @@
+//comentario de linha
+console.log("teste"); //comentario 2
+/* 
+Linhas de comentario
+comentario
+comentario
+comentario
+comentario
+*/
+console.log("teste 2"); //comentario 2
+
---
+++ JS/022- juntando arquivos js e html/index.js
@@ -0,0 +1,8 @@
+console.log("Olá mundo! 2");
+/* Formas de rodar o codigo:
+1- botao de play na canto superior direito
+2- rodar pelo terminal do vscode utilizando "node + endereço do arquivo"
+3- prompt de comando do pc usando o "node + endereço do arquivo"
+
+comando : cd entra na pasta
+*/
