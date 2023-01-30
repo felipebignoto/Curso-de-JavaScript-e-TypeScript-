5947e8e45a0e0e0629f82db80b2e03cbc1b16f37
Felipe-Bignoto <felipebignoto@gmail.com>
2023-01-30 09:40:28 -0300

    iniciar operadores logicos

---
+++ JS/039-operadores de comparação/index.js
@@ -0,0 +1,22 @@
+/*operadores de comparação
+
+>
+>=
+<
+<=
+== igualdade
+=== igualdade valor e tipo
+!=  diferentte
+!=== diferença no valor e tipo
+
+*/
+
+console.log(10>5);
+
+const comp = 5 < 4;
+console.log(comp);
+
+const num1 = 10;
+const num2 = 11;
+console.log(num1 == num2)
+
