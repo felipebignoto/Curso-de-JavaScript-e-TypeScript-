30944310af9d2c4099fb2bd6a7c2c17f84d0a6d8
Felipe-Bignoto <felipebignoto@gmail.com>
2023-03-06 09:13:35 -0300

    final de funções

--- JS/02-função/062-funções/index.js
+++ JS/02-função/062-funções/index.js
@@ -1,4 +1,17 @@
 //declaração de função -> function hoisting = a declaração vai para o inicio do codigo -> posso chamar a função antes de declarar
+falaOi();
 function falaOi(){
     console.log("Oi!");
+}
+
+//first-class objects(objetos de primeira classe) = função
+//function expression
+const souUmDado = function(){
+    console.log("Sou um dado.");
+};
+
+souUmDado();
+
+function executaFuncao(funcao){
+    funcao();
 }
