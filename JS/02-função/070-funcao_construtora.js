808ff33315ce9dbb3b0dcdda899a6b50f3b1433a
Felipe-Bignoto <felipebignoto@gmail.com>
2023-03-12 10:48:31 -0300

    iniciar aula 78

---
+++ JS/02-função/070-funcao_construtora.js
@@ -0,0 +1,22 @@
+//função construtora -> cria novo objeto, faz muita coisa automatica, retorna objetp
+//iniciar com letra maiuscula
+
+function Pessoa(nome, sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.metodo = function(){
+        console.log("Sou um metodo " + this.nome);
+    }
+
+    //atributos privados
+    const ID = 123;
+    const metodoInterno = function(){
+
+    };
+}
+
+const p1 = new Pessoa("Felipe", "Bignoto");
+const p2 = new Pessoa("Jorge","Silva");
+console.log(p1);
+console.log(p2.sobrenome);
+p1.metodo();
